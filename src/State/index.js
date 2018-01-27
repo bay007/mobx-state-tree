798403ff00a8ff6145e8856b15ed9ccb39baa756
egalicia <tnt.galicia@gmail.com>
2018-01-27 17:26:22 -0600

    Primer git

---
+++ src/State/index.js
@@ -0,0 +1,36 @@
+
+import { types as t } from 'mobx-state-tree';
+
+const WishListItem = t.model('WishListItem', {
+  name: t.optional(t.string, ''),
+  price: t.optional(t.number, 0),
+  image: t.optional(t.string, "https://www.diariofemenino.com/uploads/psicologia/188786-sonar-regalos.jpg")
+}).actions(self => ({
+  set(item) {
+    if (item.name) {
+      self.name = item.name
+    }
+    if (item.price) {
+      self.price = item.price
+    }
+    if (item.image) {
+      self.image = item.image
+    }
+  },
+}))
+
+
+const WishList = t.model('WishList', {
+  items: t.optional(t.array(WishListItem), [])
+}).actions(self => ({
+  add(WishListItem) {
+    self.items.push(WishListItem)
+  }
+}))
+
+
+const State = {
+  WishList: WishList.create()
+}
+export default State;
+
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import 'antd/dist/antd.css';
+import State from './State';
+import { observer } from "mobx-react";
+
+const App = observer(class app extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  componentDidMount() {
+    State.WishList.add({ name: 'lista_2.1', price: 1 })
+    State.WishList.add({ name: 'lista_2.2', price: 2 })
+    State.WishList.add({ name: 'lista_2.3', price: 3, image: 'https://image.jimcdn.com/app/cms/image/transf/none/path/s3d8a2bc7536f18c5/image/i79df4c739d868452/version/1482865057/image.png' })
+    State.WishList.items[0].set({ price: 142937440 })
+    State.WishList.items.map(item => { console.log(`Nombre: ${item.name}, precio $${item.price}`) })
+  }
+
+  render() {
+
+    return <div><Hello/>{State.WishList.items.map(item => (<div key={item.name}><p >Nombre: {item.name}</p><p >Price: ${item.price}</p><img style={{ width: 20 }} src={item.image}></img><hr /></div>))}</div>
+  }
+})
+
+render(<App />, document.getElementById('root'));
